b0e549be2b645b479a22cc71e6400c046d1e56b3
oscargregorius <oscar.gregorius@gmail.com>
2020-09-29 10:58:14 +0200

    Looping through realfloor numbers.

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class LuckySkyScraper {
+
+    static public void floorList(int totalRealFloors){
+        for(int realFloor = 1; realFloor <= totalRealFloors; realFloor++){
+            System.out.println("RealFloorNo: " + realFloor);
+        }
+    }
 }
