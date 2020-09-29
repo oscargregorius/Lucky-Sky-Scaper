df8799114247e18d0cdf7ef6800a9f3729d36174
oscargregorius <oscar.gregorius@gmail.com>
2020-09-29 11:17:42 +0200

    Now the list can show corresponding fake floor numbers

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -3,8 +3,12 @@ package com.company;
 public class LuckySkyScraper {
 
     static public void floorList(int totalRealFloors){
+        int fakeFloor = 1;
         for(int realFloor = 1; realFloor <= totalRealFloors; realFloor++){
-            System.out.println("RealFloorNo: " + realFloor);
+            System.out.println("RealFloorNo: " + realFloor + " fakeFloor: " + fakeFloor);
+            do {
+                fakeFloor++;
+            }while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
         }
     }
 }
